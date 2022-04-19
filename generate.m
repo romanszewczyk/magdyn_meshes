b2cd9fe67d9e451fcad5241d0d92fcc592d6c530
Roman Szewczyk <rszewczyk@onet.pl>
2022-04-19 13:20:42 +0200

    Update generate.m

--- generate.m
+++ generate.m
@@ -23,7 +23,7 @@
 %
 % DESCRIPTION:
 % Script generating .msz file connected with the paper:
-% A. Ostaszewska-Li¿ewska, D. Kopala, R. Szewczyk
+% A. Ostaszewska-Lizewska, D. Kopala, R. Szewczyk
 % "Improved control of mesh density in adaptive tetrahedral meshes 
 % for finite element modeling"
 % submitted to Measurement Automation Robotics Journal
